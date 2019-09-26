c124464f2b9772deee2755cc60daad176af117c4
Felix Martinez <felixmanuel396@gmail.com>
2019-09-26 08:40:10 -0400

    Modificaciones en los modulos y rutas

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Charts1Component } from './charts1/charts1.component';
+
 
 const routes: Routes = [
-
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'charts1', component: Charts1Component },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ]
+  },
 ];
 
 @NgModule({
